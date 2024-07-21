1274f380d8eaf70ea7a5526bc34aa47f3beb4ca7
PrincessPandaSource <Lisahuang2003@gmail.com>
2024-07-21 11:46:40 -0500

    Updating website now that Github deploys it

--- Scripts/footer.js
+++ Scripts/footer.js
@@ -2,6 +2,6 @@ document.write('\
 \
   <p>Copyright © 2022-2024 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION. Other copyrighted properties are by their respective owners.</p>\
   <p>All original content may be reproduced and/or altered with attribution and for noncommercial purposes that are not abusive or illegal.</p>\
-  <p>Hosted by <a href="https://www.netlify.com/">Netlify</a></p>\
+  <p>Hosted by <a href="https://www.netlify.com/">Netlify</a>, deployed from <a href="https://github.com/PrincessPandaSource/PrincessPandaLover-official-website">public Github repository</a> (meaning this website is open source)</p>\
 \
 ');
