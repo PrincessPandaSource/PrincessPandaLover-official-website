b65b68ba4f3fcbbe926494179665a29a26e11ad5
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-07-04 19:34:51 -0500

    Updating footers

--- SilverScripts/js/comic_footer.js
+++ SilverScripts/js/comic_footer.js
@@ -2,7 +2,7 @@
 //at the bottom of your page, but before the js script calls and the closing body tag, put an empty div with a class of "writeFooter"
 document.querySelector(".writeFooter").innerHTML = `
     <footer>
-        <p>Copyright © 2024 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION.</p>
+        <p>Copyright © 2024-2025 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION.</p>
         <p>All content may be reproduced and/or altered with attribution and for noncommercial purposes that are not abusive or illegal.</p>
         <p>Website template:</p>
         <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a><br/><span style="font-size:0.8em">(Of course, I could've coded the whole thing myself, but when I discovered this template, I was like "This is a godsend! I NEED to use it!".)</span>
--- sonicverse/Scripts/sonicHeader.js
+++ sonicverse/Scripts/sonicHeader.js
@@ -12,8 +12,8 @@ document.write('\
       <li class="sidebarLink"><a href="">AUs</a></li>\
       <li class="sidebarLink"><a href="/sonicverse/FAQ.html">FAQ</a></li>\
       <li class="sidebarLink"><a href="/index.html">PrincessPandaLover Home</a></li>\
-      <p style="font-size: 10px; padding: 10px;">Copyright © 2022-2024 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION.<br/><br/>\
-      All content may be reproduced and/or altered with attribution and for noncommercial purposes that are not abusive or illegal.<br/><br/>\
+      <p style="font-size: 10px; padding: 10px;">Copyright © 2022-2025 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION.<br/><br/>\
+      All original content may be reproduced and/or altered with attribution and for noncommercial purposes that are not abusive or illegal.<br/><br/>\
       <b>DISCLAIMER</b>: This documents a fan version of <i>Sonic</i> contiunities that is not considered official by Sega. For official information about <i>Sonic</i>, please seek <a href="https://sonic.fandom.com">Sonic Wiki Zone</a>.<br/><br/>\
       Hosted by <a href="https://www.netlify.com/">Netlify</a></p>\
   </ul>\
