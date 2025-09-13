6ef6c22edb27c0199f7ef6435829eb05ac3c6854
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-09-13 10:26:12 -0500

    LAZY LOADING, LAZY LOADING EVERYWHERE

--- Scripts/logo.js
+++ Scripts/logo.js
@@ -1,5 +1,5 @@
 document.querySelector(".logo").innerHTML ='\
 \
-<a href="/index.html" class="colored-link"><img src="/Images/Logo.svg" class="logo" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
+<a href="/index.html" class="colored-link"><img src="/Images/Logo.svg" class="logo" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'  loading="lazy"></a>\
 \
 ';
