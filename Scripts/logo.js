eb4e0f381ff45b491e193044f370845a93094b16
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-09-14 10:44:45 -0500

    Adding width and height elements to images to avoid layout shifts, adjusting background CSS to avoid inconsistency on pages

--- Scripts/logo.js
+++ Scripts/logo.js
@@ -1,5 +1,5 @@
 document.querySelector(".logo").innerHTML ='\
 \
-<a href="/" class="colored-link"><img src="/Images/Logo.svg" class="logo" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
+<a href="/" class="colored-link"><img src="/Images/Logo.svg" class="logo" width="396" height="112" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
 \
 ';
