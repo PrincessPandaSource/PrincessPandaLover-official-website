82a2418ccc49904f42c9b9935837ea0be46ce797
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-09-13 23:39:55 -0500

    Cleaning up "index.html" links, removing redirect for that, bolding header text for accessibility

--- Scripts/logo.js
+++ Scripts/logo.js
@@ -1,5 +1,5 @@
 document.querySelector(".logo").innerHTML ='\
 \
-<a href="/index.html" class="colored-link"><img src="/Images/Logo.svg" class="logo" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
+<a href="/" class="colored-link"><img src="/Images/Logo.svg" class="logo" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
 \
 ';
