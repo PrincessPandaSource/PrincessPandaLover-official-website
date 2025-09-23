f657eb793e38013f7fa9bbad3a1cb6790ee5b9bb
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-09-23 18:39:00 -0500

    Renaming page URLs to hyphens for better look, and adding redirects

--- tpt2/articleSelect.js
+++ tpt2/articleSelect.js
@@ -1,9 +1,9 @@
 document.querySelectorAll(".articleSelect").forEach((element) => element.innerHTML = `
     <label for="article-select" style="color:black;">Select month:</label>
     <select id="article-select">
-    <option value="July_2024.html">July 2024</option>
-    <option value="June_2024.html">June 2024</option>
-    <option value="May_2024.html">May 2024</option>
+    <option value="July-2024.html">July 2024</option>
+    <option value="June-2024.html">June 2024</option>
+    <option value="May-2024.html">May 2024</option>
     </select>
     <button id="go-to-article" onclick="goToArticle()">Go</button>
 `);
