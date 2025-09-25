9c5e3338855fde8d896d9785c31dab1dc3523fcb
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-09-24 19:23:59 -0500

    Lowercase folder and page names

--- Scripts/logo.js
+++ Scripts/logo.js
@@ -1,5 +1,5 @@
 document.querySelector(".logo").innerHTML ='\
 \
-<a href="/" class="colored-link"><img src="/Images/Logo.svg" class="logo" width="396" height="112" fetchpriority="high" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
+<a href="/" class="colored-link"><img src="/images/Logo.svg" class="logo" width="396" height="112" fetchpriority="high" alt=\'"PrincessPandaLover" logo colored in magenta and with heart at beginning\'></a>\
 \
 ';
