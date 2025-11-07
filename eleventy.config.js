a2319721e3e40b1c8c53a1d5093c4c39d4b658ed
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-11-06 20:48:59 -0600

    Fixing Open Graph and Twitter card issues, standarizing includes organization, fixing header in Silver Scripts site

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,7 @@ export default function (eleventyConfig) {
     // Import pre-existing resources to build
     eleventyConfig.addPassthroughCopy("images/*.svg");
     eleventyConfig.addPassthroughCopy("images/*.ico");
+    eleventyConfig.addPassthroughCopy("images/logo.png")
     eleventyConfig.addPassthroughCopy("images/background.png")
     eleventyConfig.addPassthroughCopy("images/link-icons");;
     eleventyConfig.addPassthroughCopy("styles");
