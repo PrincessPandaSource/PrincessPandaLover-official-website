cc4d860f5302a1eeff1d263cafd5bdb9f09e219a
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-11-11 12:38:04 -0600

    Further accessibility fixes, changing link colors

--- silverscripts/js/comic_footer.js
+++ silverscripts/js/comic_footer.js
@@ -5,7 +5,7 @@ document.querySelector(".writeFooter").innerHTML = `
         <p>Copyright © 2024-2025 by PrincessPandaLover. <i>Sonic the Hedgehog</i> is copyrighted © by SEGA CORPORATION.</p>
         <p>All content may be reproduced and/or altered with attribution and for noncommercial purposes that are not abusive or illegal.</p>
         <p>Website template:</p>
-        <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" /></a><br/><span style="font-size:0.8em">(Of course, I could've coded the whole thing myself, but when I discovered this template, I was like "This is a godsend! I NEED to use it!".)</span>
+        <a href="https://rarebit.neocities.org"><img src="img/rarebitlogo_small.png" height = "30" alt="Rarebit logo" /></a><br/><span style="font-size:0.8em">(Of course, I could've coded the whole thing myself, but when I discovered this template, I was like "This is a godsend! I NEED to use it!".)</span>
         <p><a href="https://github.com/PrincessPandaSource/PrincessPandaLover-official-website">Source code on GitHub</a></p>
     </footer>
 `;
