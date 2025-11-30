90405bd04c1c5dda3bde25a43588ff2e36af8311
PrincessPandaSource <Lisahuang2003@gmail.com>
2025-11-29 18:21:24 -0600

    Converting bio section to h-card (lotsa span tags)

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,8 +8,9 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images/*.svg");
     eleventyConfig.addPassthroughCopy("images/*.ico");
     eleventyConfig.addPassthroughCopy("images/logo.png")
-    eleventyConfig.addPassthroughCopy("images/background-accessible.png")
-    eleventyConfig.addPassthroughCopy("images/link-icons");;
+    eleventyConfig.addPassthroughCopy("images/background-accessible.png");
+    eleventyConfig.addPassthroughCopy("images/avatar.png");
+    eleventyConfig.addPassthroughCopy("images/link-icons");
     eleventyConfig.addPassthroughCopy("styles");
     eleventyConfig.addPassthroughCopy("scripts");
     eleventyConfig.addPassthroughCopy("fonts");
